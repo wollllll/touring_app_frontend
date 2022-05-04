906dd2d6fa3a446b2d831390f1bb5c43da1364aa
wollllll <sakin50guramu@gmail.com>
2022-05-04 19:47:52 +0900

    header修正

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -4,17 +4,19 @@ console.log(APP_NAME)
 </script>
 
 <template>
-  <header class="grid grid-cols-2 bg-blue-300">
-    <div class="flex items-center justify-start p-3 font-bold">
-      <router-link :to="{ name: 'top' }">{{ APP_NAME }}</router-link>
+  <header class="grid grid-cols-2 px-2 bg-blue-300">
+    <div class="flex items-center justify-start">
+      <router-link :to="{ name: 'top' }" class="font-bold">
+        {{ APP_NAME }}
+      </router-link>
     </div>
-    <div class="flex items-center justify-end p-3">
+    <div class="flex items-center justify-end">
       <router-link :to="{ name: 'top' }">
-        <i class="bi bi-plus-square md:text-2xl sm:text-xl" />
+        <i class="bi bi-plus-square text-2xl" />
       </router-link>
       <label for="my-drawer" tabindex="0" class="pl-5">
         <img
-          class="btn btn-sm btn-ghost btn-circle avatar"
+          class="btn btn-sm btn-ghost btn-circle avatar object-cover"
           alt="ユーザーicon"
           src="https://api.lorem.space/image/face?hash=33791"
         />
